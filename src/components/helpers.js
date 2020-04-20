607ebefad1ba4595a68551f1eb89c1fb7ab141be
Bradley Kenyon <b@bradleykenyon.com>
2020-04-20 10:54:28 -0700

    setting up charts

--- src/components/helpers.js
+++ src/components/helpers.js
@@ -1,3 +1,32 @@
 export const PercentDiff = (initial, current, precision = 2) => {
   return (((current-initial)/initial)*100).toFixed(precision);
 }
+
+export const DataFormatter = (dataArray, segment) => {
+  const addDeltasArray = dataArray.map( ({node}, index, array) =>  {
+    let delta = null
+
+    if (index > 0) {
+      delta = node[segment] - array[index - 1].node[segment]
+    }
+
+    return {
+      ...node,
+      rawDataPoint: parseInt(node[segment], 0),
+      dailyDelta: delta
+    }
+  })
+
+  return addDeltasArray.map( (node, index, array) => {
+    let movingAverage = null
+
+    if (index > 5) {
+      movingAverage = (array.slice(index - 5, index).reduce((total, node) => total + parseInt(node.dailyDelta), 0)) / 5
+    }
+
+    return {
+      ...node,
+      movingAverage: movingAverage
+    }
+  })
+}
--- src/pages/confirmed-cases.js
+++ src/pages/confirmed-cases.js
@@ -1,16 +1,37 @@
 import React from "react"
 import { graphql } from "gatsby"
+import { DataFormatter } from '../components/helpers'
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import ChartBar from '../components/chartBar'
+import ChartCombo from '../components/chartCombo'
 
 export default ({ data }) => {
+
+  const highestRawData = data.allGoogleSpreadsheetRawData.edges.reduce((max, node) => max.confirmedCases > node.confirmedCases ? max : node)
+  const formattedData = DataFormatter(data.allGoogleSpreadsheetRawData.edges, 'confirmedCases')
+
   return (
     <Layout>
       <SEO title="Confirmed Cases" />
 
-      <h2>page2</h2>
+      <h1 className="main-content__title main-content__layout--header">Confirmed Cases</h1>
 
+      <div className="main-content__layout--charts">
+        <ChartBar
+          chartTitle="Daily Total Reported"
+          segment="confirmed-cases"
+          content={formattedData}
+          highValue={highestRawData.node.confirmedCases}
+        />
+
+        <ChartCombo
+          chartTitle="Daily Change with 5-day Moving Average"
+          segment="confirmed-cases"
+          content={formattedData}
+        />
+      </div>
     </Layout>
   )
 }
@@ -20,7 +41,7 @@ export const query = graphql`
     allGoogleSpreadsheetRawData {
       edges {
         node {
-          dateReported
+          dateReported(formatString: "MMMM DD, YYYY", locale: "us")
           confirmedCases
         }
       }
