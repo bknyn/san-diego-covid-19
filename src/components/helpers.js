f805ceb2c58f7a5e727a9d48d6613913d6731bbe
Bradley Kenyon <b@bradleykenyon.com>
2020-04-21 13:53:18 -0700

    adding string helper for slug and camelcase

--- src/components/helpers.js
+++ src/components/helpers.js
@@ -2,6 +2,19 @@ export const PercentDiff = (initial, current, precision = 2) => {
   return (((current-initial)/initial)*100).toFixed(precision);
 }
 
+export const StringFormatter = (title, format = 'slug') => {
+  switch(format) {
+    case 'slug':
+      // lower case and replace spaces with hyphens
+      return title.toLowerCase().replace(/\s+/g, '-')
+    case 'camelCase':
+      // lower case, find spaces and convert follow character to upper case
+      return title.toLowerCase().replace(/\s+([a-z])/g, (g) => g[1].toUpperCase())
+    default:
+      return title
+  }
+}
+
 export const DataFormatter = (dataArray, segment) => {
   const addDeltasArray = dataArray.map( ({node}, index, array) =>  {
     let delta = null
