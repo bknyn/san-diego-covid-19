926692dfbdff51b71f1e2936dd6afcc30ed5312c
Bradley Kenyon <b@bradleykenyon.com>
2020-04-23 10:45:22 -0700

    Add overall rates to page data

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,13 +5,21 @@ import Layout from '../components/layout'
 import SEO from '../components/seo'
 
 export default ({ data }) => {
+  const pageData = data.allGoogleSpreadsheetRawData.edges.map(({node}) => {
+    return {
+      ...node,
+      hospitalizedRate: ((node.hospitalized / node.confirmedCases) * 100).toFixed(2),
+      icuRate: ((node.icu / node.confirmedCases) * 100).toFixed(2),
+      deathRate: ((node.deaths / node.confirmedCases) * 100).toFixed(2)
+    }
+  })
+
+  console.log(pageData)
 
   return (
     <Layout>
       <SEO title="Confirmed Cases" />
-
       <h1 className="title--main main-content__layout--header">Overview</h1>
-
     </Layout>
   )
 }
