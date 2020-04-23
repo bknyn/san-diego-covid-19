708da897f056d7bfeae002408dfe64a9eb3803dc
Bradley Kenyon <b@bradleykenyon.com>
2020-04-22 19:11:02 -0700

    stripping index and adding data to query

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,10 @@
-import React from "react"
-import { graphql } from "gatsby"
-import { PercentDiff, DataFormatter } from '../components/helpers'
+import React from 'react'
+import { graphql } from 'gatsby'
 
-import Layout from "../components/layout"
-import SEO from "../components/seo"
-import ChartBar from '../components/chartBar'
-import ChartCombo from '../components/chartCombo'
-import Scorecard from "../components/scorecard"
+import Layout from '../components/layout'
+import SEO from '../components/seo'
 
 export default ({ data }) => {
-  const highestRawData = data.allGoogleSpreadsheetRawData.edges.reduce((max, node) => max.confirmedCases > node.confirmedCases ? max : node)
-  const formattedData = DataFormatter(data.allGoogleSpreadsheetRawData.edges, 'confirmedCases')
-  const lastThreeDays = formattedData.slice(formattedData.length - 3, formattedData.length)
 
   return (
     <Layout>
@@ -19,37 +12,6 @@ export default ({ data }) => {
 
       <h1 className="title--main main-content__layout--header">Confirmed Cases</h1>
 
-      <div className="main-content__layout--charts">
-        <ChartBar
-          chartTitle="Daily Total Reported"
-          segment="confirmed-cases"
-          content={formattedData}
-          highValue={highestRawData.node.confirmedCases}
-        />
-
-        <ChartCombo
-          chartTitle="Daily Change with 5-day Moving Average"
-          segment="confirmed-cases"
-          content={formattedData}
-        />
-      </div>
-
-      <div className="main-content__layout--scorecards">
-        <h2 className="title--section">Today vs Yesterday</h2>
-        <Scorecard
-          title="Total Cases"
-          bigNumber={lastThreeDays[2].rawDataPoint}
-          comparison={lastThreeDays[2].dailyDelta}
-        />
-
-        <Scorecard
-          title="Growth Rate"
-          bigNumber={`${PercentDiff(lastThreeDays[1].rawDataPoint, lastThreeDays[2].rawDataPoint)}%`}
-          comparison={
-            `${(PercentDiff(lastThreeDays[1].rawDataPoint, lastThreeDays[2].rawDataPoint) - PercentDiff(lastThreeDays[0].rawDataPoint, lastThreeDays[1].rawDataPoint)).toFixed(2)}%`
-          }
-        />
-      </div>
     </Layout>
   )
 }
@@ -59,8 +21,11 @@ export const query = graphql`
     allGoogleSpreadsheetRawData {
       edges {
         node {
-          dateReported(formatString: "MMMM DD, YYYY")
           confirmedCases
+          dateReported(formatString: "MMMM DD, YYYY")
+          deaths
+          hospitalized
+          icu
         }
       }
     }
