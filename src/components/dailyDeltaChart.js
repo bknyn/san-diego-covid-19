440a88ab87467b6ec53bf9519dcb15cef0353f26
Bradley Kenyon <b@bradleykenyon.com>
2020-04-28 21:26:02 -0700

    rechart moving averages

--- src/components/dailyDeltaChart.js
+++ src/components/dailyDeltaChart.js
@@ -1,13 +1,21 @@
 import React from "react"
+import { CalculateMovingAverage } from './helpers'
 import { ResponsiveContainer, ComposedChart, XAxis, YAxis, CartesianGrid, Bar, Line, Tooltip } from 'recharts'
 import sassVars from '../styles/principles/_variables.scss'
 
 const DailyDeltaChart = ({ segment, content }) => {
+  const contentWithMovingAverages = content.map( (node, index, array) => {
+    return {
+      ...node,
+      sevenDayMA: index >= 6 ? CalculateMovingAverage(array, index, 7) : null
+    }
+  })
+
   return (
     <div className="chart chart--combo">
-      <h2 className="text-centered">Daily Change</h2>
+      <h2 className="text-centered">Daily Change (& 7-day MA)</h2>
       <ResponsiveContainer width="100%" height={350}>
-        <ComposedChart data={content}>
+        <ComposedChart data={contentWithMovingAverages}>
           <XAxis dataKey="dateReported" />
           <YAxis type="number" allowDataOverflow={true} />
           <Tooltip />
@@ -17,7 +25,14 @@ const DailyDeltaChart = ({ segment, content }) => {
             name={segment.title}
             fill={sassVars[segment.camelCaseKey + 'Color']}
           />
-          <Line type="monotone" dataKey="movingAverage" stroke="#333" />
+          <Line
+            type="monotone"
+            dataKey="sevenDayMA"
+            name="7-day Moving Average"
+            stroke="#333"
+            strokeWidth="2"
+            dot={false}
+          />
         </ComposedChart>
       </ResponsiveContainer>
     </div>
--- src/components/helpers.js
+++ src/components/helpers.js
@@ -1,3 +1,9 @@
+export const CalculateMovingAverage = (array, index, days) => {
+  const slicedArray = array.slice(index - (days - 1), index + 1)
+  const maValue = slicedArray.reduce((total, node) => total + node.dailyDelta, 0) / days
+  return maValue.toFixed(2)
+}
+
 export const PercentDiff = (initial, current, precision = 2) => {
   return (((current-initial)/initial)*100).toFixed(precision);
 }
@@ -13,33 +19,4 @@ export const StringFormatter = (title, format = 'slug') => {
     default:
       return title
   }
-}
-
-export const DataFormatter = (dataArray, segment) => {
-  const addDeltasArray = dataArray.map( ({node}, index, array) =>  {
-    let delta = null
-
-    if (index > 0) {
-      delta = node[segment] - array[index - 1].node[segment]
-    }
-
-    return {
-      ...node,
-      rawDataPoint: parseInt(node[segment], 0),
-      dailyDelta: delta
-    }
-  })
-
-  return addDeltasArray.map( (node, index, array) => {
-    let movingAverage = null
-
-    if (index > 5) {
-      movingAverage = (array.slice(index - 5, index).reduce((total, node) => total + parseInt(node.dailyDelta), 0)) / 5
-    }
-
-    return {
-      ...node,
-      movingAverage: movingAverage
-    }
-  })
 }
